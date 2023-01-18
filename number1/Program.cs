74b8f65351f0cfddc7686d4403c33737b1279004
Oleg <zerafperon@gmail.com>
2023-01-18 16:27:51 +0400

    изменил number1

--- number1/Program.cs
+++ number1/Program.cs
@@ -2,14 +2,56 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-int a = 2;
+int a = 5;
 int b = 7;
-
-if (a > b)
+if (a < b)
 {
-Console.WriteLine(b);
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(b);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(a);
 }
 else
 {
-Console.WriteLine(a);
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(a);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(b);
 }
+
+
+int a1 = 2;
+int b1 = 10;
+if (a < b)
+{
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(b1);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(a1);
+}
+else
+{
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(a1);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(b1);
+}
+
+
+int a2 = -9;
+int b2 = -3;
+if (a < b)
+{
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(b2);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(a2);
+}
+else
+{
+    Console.WriteLine("Большее число:");
+    Console.WriteLine(a2);
+    Console.WriteLine("Меньшее число:");
+    Console.WriteLine(b2);
+}
+
