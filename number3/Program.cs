d9c6a9bd53e51da7cd4ab4855a2b66bf6bb504ba
Oleg <zerafperon@gmail.com>
2023-01-18 17:05:11 +0400

    Изменил number3

---
+++ number3/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+int a = 4;
+int number = a;
+if (number % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    System.Console.WriteLine("Это нечетное число");
+}
+int b = -3;
+int number1 = b;
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    System.Console.WriteLine("Это нечетное число");
+}
+int c = 7;
+int number2 = c;
+
+if (number2 % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    System.Console.WriteLine("Это нечетное число");
+}
